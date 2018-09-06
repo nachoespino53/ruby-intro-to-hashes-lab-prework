43a307e6957a03b57714fd53280e499d3a8224e6
Ignacio R Espino <irespino@capozios.com>
2018-09-06 18:32:16 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,10 @@ def monopoly_with_second_tier
 	monopoly = {
 	  :railroads => {
 	    :pieces => 4,
-	    :names => {}
+	    :names => {},
+	    :rent_in_dollars => {}
+	      
+	    }
 	  }
 	}
 	
